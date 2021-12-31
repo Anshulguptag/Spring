2a2237d563025248db5a806f66a8f9a6839c554a
Anshulguptag <anshulguptamasteramazon@gmail.com>
2021-12-31 19:18:54 +0530

    created blog posts api

---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/PostServiceImpl.java
@@ -0,0 +1,36 @@
+package com.springboot.blog.springbootblogrestapi.service.impl;
+
+import com.springboot.blog.springbootblogrestapi.entity.Post;
+import com.springboot.blog.springbootblogrestapi.payload.PostDto;
+import com.springboot.blog.springbootblogrestapi.repository.PostRepository;
+import com.springboot.blog.springbootblogrestapi.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository){
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post newPost = postRepository.save(post);
+
+        //Convert entity to DTO
+        PostDto postResponse = new PostDto();
+        postResponse.setId(newPost.getId());
+        postResponse.setDescription(newPost.getDescription());
+        postResponse.setTitle(newPost.getTitle());
+        postResponse.setContent(newPost.getContent());
+
+        return postResponse;
+    }
+}
